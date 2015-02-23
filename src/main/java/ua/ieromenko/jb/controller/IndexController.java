1532625311b6e36c2cf0ade3a73fe467d216d039
Alex-e <a.ieromenko@gmail.com>
2015-02-23 22:04:40 +0200

    add apache tiles

--- src/main/java/ua/ieromenko/jb/controller/IndexController.java
+++ src/main/java/ua/ieromenko/jb/controller/IndexController.java
@@ -2,13 +2,13 @@ package ua.ieromenko.jb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+// 4 
 @Controller
 public class IndexController {
 
 	@RequestMapping("/index")
 	public String index(){
-		return "/WEB-INF/jsp/index.jsp";
+		return "index";
 		
 	}
 	
