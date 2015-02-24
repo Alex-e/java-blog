5ed438bea1b63a7a3eee435f7d3a2fc59329d689
Alex-e <a.ieromenko@gmail.com>
2015-02-24 16:47:56 +0200

    add applicationcontext listener

--- src/main/java/ua/ieromenko/jb/controller/IndexController.java
+++ src/main/java/ua/ieromenko/jb/controller/IndexController.java
@@ -2,7 +2,7 @@ package ua.ieromenko.jb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-// 4 
+
 @Controller
 public class IndexController {
 
