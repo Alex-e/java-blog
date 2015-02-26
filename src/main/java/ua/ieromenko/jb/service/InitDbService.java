3690d64ebf037eabf1e97377bc6c414cd16be06d
Alex-e <a.ieromenko@gmail.com>
2015-02-26 15:35:41 +0200

    add db initializer

---
+++ src/main/java/ua/ieromenko/jb/service/InitDbService.java
@@ -0,0 +1,77 @@
+package ua.ieromenko.jb.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ua.ieromenko.jb.entity.Blog;
+import ua.ieromenko.jb.entity.Item;
+import ua.ieromenko.jb.entity.Role;
+import ua.ieromenko.jb.entity.User;
+import ua.ieromenko.jb.repository.BlogRepository;
+import ua.ieromenko.jb.repository.ItemRepository;
+import ua.ieromenko.jb.repository.RoleRepository;
+import ua.ieromenko.jb.repository.UserRepository;
+
+@Transactional
+@Service
+public class InitDbService {
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private BlogRepository blogRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	@PostConstruct
+	public void init(){
+		Role roleUser = new Role();
+		roleUser.setName("ROLE_USER");
+		roleRepository.save(roleUser);
+		
+		Role roleAdmin = new Role();
+		roleAdmin.setName("ROLE_ADMIN");
+		roleRepository.save(roleAdmin);
+		
+		User userAdmin = new User();
+		userAdmin.setName("admin");
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(roleAdmin);
+		roles.add(roleUser);
+		userAdmin.setRoles(roles);
+		userRepository.save(userAdmin);
+		
+		Blog blogJavavids = new Blog();
+		blogJavavids.setName("JavaVids");
+		blogJavavids.setUser(userAdmin);
+		blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+		blogRepository.save(blogJavavids);
+		
+		Item item1 = new Item();
+		item1.setBlog(blogJavavids);
+		item1.setTitle("First");
+		item1.setLink("http://www.javavids.com");
+		item1.setPublishedDate(new Date());
+		itemRepository.save(item1);
+
+		Item item2 = new Item();
+		item2.setBlog(blogJavavids);
+		item2.setTitle("First");
+		item2.setLink("http://www.javavids.com");
+		item2.setPublishedDate(new Date());
+		itemRepository.save(item2);
+	}
+
+}
