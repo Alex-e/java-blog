f1b5afc4dea1c956a9f2466c00ad28eea32a99c4
Alex-e <a.ieromenko@gmail.com>
2015-03-03 14:18:08 +0200

    add registration form

--- src/main/java/ua/ieromenko/jb/controller/UserController.java
+++ src/main/java/ua/ieromenko/jb/controller/UserController.java
@@ -3,9 +3,11 @@ package ua.ieromenko.jb.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ua.ieromenko.jb.entity.User;
 import ua.ieromenko.jb.service.UserService;
 
 @Controller
@@ -13,6 +15,11 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@ModelAttribute("user")
+	public User construct(){
+		return new User();
+	}
 	
 	@RequestMapping("/users")
 	public String users(Model model){
--- src/main/webapp/WEB-INF/layout/taglib.jsp
+++ src/main/webapp/WEB-INF/layout/taglib.jsp
@@ -2,7 +2,7 @@
     pageEncoding="UTF-8"%>
 
     <%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
-    <%@   taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
-    <%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"  %>
     <%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
     <%@ taglib uri="http://tiles.apache.org/tags-tiles-extras" prefix="tilesx" %>
+    <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"  %>
